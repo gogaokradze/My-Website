af4226dbd6040e2febcb60ac2f30ea84dbef0126
Goga Okradze <deathofsuper@gmail.com>
2022-02-04 13:59:53 +0400

    Changed title

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
     <>
     <div className='w-2/3 mx-auto'>
         <div className='lg:ml-8 '>
-          <span className="inline-flex p-3 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-accent" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path></svg></span>
+          <span className="inline-flex p-3 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-accent" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path></svg></span>
           <h1 className="text-2xl">Projects</h1>
           <p>Here are some of my projects</p>
         </div>
